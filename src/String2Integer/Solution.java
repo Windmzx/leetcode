76c1ba1cbde454073b744ca4c6393984f5872938
Windmzx <2281927774@qq.com>
2018-12-07 10:07:41 +0800

    String2Integer

--- src/String2Integer/Solution.java
+++ src/String2Integer/Solution.java
@@ -4,39 +4,33 @@ public class Solution {
 
 
     public int myAtoi(String str) {
-        boolean isvavild = false;//true if number if discover
-        int res = 0;
+        char[] s = str.toCharArray();
+        int i = 0;
         int id = 1;
-        for (int i = 0; i < str.length(); i++) {
-            char temp = str.charAt(i);
-            if ((temp == '-' || temp == ' ' || temp == '+') && !isvavild || (temp - '0' >= 0 && temp - '0' <= 9)) {
-                if (temp == ' ' || temp == '+')
-                    continue;
-                else if (temp == '-') {
-                    id = -1;
-                } else if (!(temp - '0' >= 0 && temp - '0' <= 9)) {
-                    return res;
-                } else {
-                    isvavild = true;
-                    int p = res * 10 + temp - '0';
-                    if (p / 10 != res) {
-                        return id > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                    }
-                    res = p;
-                }
-            } else if (!isvavild) {
-                return 0;
-            } else
-                break;
-
+        int res = 0;
+        while (i<s.length&&s[i] == ' ') i++;
+        if (i >= s.length) {
+            return 0;
         }
-        if (isvavild)
-            return res * id;
-        return 0;
+        if (s[i] == '-') {
+            i++;
+            id = -1;
+        }else if (s[i] == '+') {
+            i++;
+            id = 1;
+        }
+        while (i < str.length() && s[i] >= '0' && s[i] <= '9') {
+            int p = res * 10 + s[i] - '0';
+            if (p / 10 != res)
+                return id > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            i++;
+            res = p;
+        }
+        return res * id;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.myAtoi("+1"));
+        System.out.println(solution.myAtoi("   -"));
     }
 }
